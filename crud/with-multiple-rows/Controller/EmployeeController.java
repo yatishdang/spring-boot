ebfe4d523bbdb25f4f336be7087497aaa58e8e5e
yatishdang <yatishdang@gmail.com>
2023-12-18 00:32:59 +0530

    Create EmployeeController.java

---
+++ crud/with-multiple-rows/Controller/EmployeeController.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/saveEntities")
+    public void saveEntities(@RequestBody List<EmployeeEntity> entities) {
+        employeeService.saveEntities(entities);
+    }
+}
